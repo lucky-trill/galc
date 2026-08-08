0a2cc966e7aace1a9ce401f98d04b9d589b05960
lucky-trill <109553278+lucky-trill@users.noreply.github.com>
2026-08-08 02:14:57 -0700

    Fixed abs elev 0 issues; added preloading

--- scripts/galc.js
+++ scripts/galc.js
@@ -3,15 +3,19 @@ const sceneDefault = "on";
 const absDefault = "on";
 const levelDefault = "included";
 const navDefault = "visible";
+const notifyDefault = false;
 const displayDefault = "no-one";
+const elevationDefault = "no";
 const hudDefault = "right";
 const delayDefault = 600;
+const preloadDefault = false;
 
 Hooks.once("init", () => {
   game.settings.register(id, 'defaultSB', {
     name: "galc.settings.defaultSB.label",
     hint: "galc.settings.defaultSB.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: sceneDefault,
     requiresReload: true,
@@ -27,6 +31,7 @@ Hooks.once("init", () => {
     name: "galc.settings.defaultAE.label",
     hint: "galc.settings.defaultAE.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: absDefault,
     requiresReload: true,
@@ -42,6 +47,7 @@ Hooks.once("init", () => {
     name: "galc.settings.defaultLI.label",
     hint: "galc.settings.defaultLI.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: levelDefault,
     requiresReload: true,
@@ -57,6 +63,7 @@ Hooks.once("init", () => {
     name: "galc.settings.defaultLN.label",
     hint: "galc.settings.defaultLN.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: navDefault,
     requiresReload: true,
@@ -68,10 +75,21 @@ Hooks.once("init", () => {
       required: true
     })
   });
+  game.settings.register(id, 'notify', {
+    name: "galc.settings.notify.label",
+    hint: "galc.settings.notify.hint",
+    localize: true,
+    scope: "world",
+    config: true,
+    default: notifyDefault,
+    requiresReload: false,
+    type: new foundry.data.fields.BooleanField()
+  });
   game.settings.register(id, 'navDisplay', {
     name: "galc.settings.navDisplay.label",
     hint: "galc.settings.navDisplay.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: displayDefault,
     requiresReload: true,
@@ -84,16 +102,34 @@ Hooks.once("init", () => {
       required: true
     })
   });
+  game.settings.register(id, 'navElevation', {
+    name: "galc.settings.navElevation.label",
+    hint: "galc.settings.navElevation.hint",
+    localize: true,
+    scope: "world",
+    config: true,
+    default: elevationDefault,
+    requiresReload: true,
+    type: new foundry.data.fields.StringField({
+      choices: {
+        "no": "galc.options.noElev",
+        "gm": "galc.options.gmElev",
+        "everyone": "galc.options.everyoneElev"
+      },
+      required: true
+    })
+  });
   game.settings.register(id, 'delay', {
     name: "galc.settings.delay.label",
     hint: "galc.settings.delay.hint",
     localize: "true",
+    scope: "world",
     config: true,
     type: Number,
     range: {
       min: 50,
       step: 50,
-      max: 10000
+      max: 6000
     },
     default: delayDefault,
     requiresReload: false
@@ -102,6 +138,7 @@ Hooks.once("init", () => {
     name: "galc.settings.hud.label",
     hint: "galc.settings.hud.hint",
     localize: true,
+    scope: "world",
     config: true,
     default: hudDefault,
     requiresReload: true,
@@ -114,6 +151,16 @@ Hooks.once("init", () => {
       },
       required: true
     })
+  });
+  game.settings.register(id, 'preload', {
+    name: "galc.settings.preload.label",
+    hint: "galc.settings.preload.hint",
+    localize: true,
+    scope: "world",
+    config: true,
+    default: preloadDefault,
+    requiresReload: false,
+    type: new foundry.data.fields.BooleanField()
   });
   game.keybindings.register(id, "notify", {
     name: "galc.keybinding.notify.label",
@@ -195,7 +242,7 @@ function renderSceneConfig(app, html, context, options) {
 Hooks.on("renderSceneConfig", renderSceneConfig);
 
 Hooks.on("updateScene", async (scene, change, options) => {
-  if (change?.flags[`${id}`]?.abs) {
+  if (scene && change?.flags[`${id}`]?.abs) {
     await canvas.draw();
   }
 });
@@ -273,8 +320,9 @@ function renderLevelConfig(app, html, context, options) {
 
 Hooks.on("renderLevelConfig", renderLevelConfig);
 
-Hooks.on("updateToken", async (token, change, options) => {
-  if (!isNaN(token?.elevation) && !isNaN(change?.elevation) && !change?.level) {
+Hooks.on("updateToken", async (token, change, options, userId) => {
+  const user = game.user;
+  if (user.id === userId && !isNaN(token?.elevation) && !isNaN(change?.elevation) && !change?.level) {
     const defaultSB = game.settings.get(id, 'defaultSB') || sceneDefault;
     const scene = token.parent;
     const flag = scene.flags[`${id}`]?.behavior || defaultSB;
@@ -283,26 +331,37 @@ Hooks.on("updateToken", async (token, change, options) => {
       const level = scene.levels.find(l => l.id === token.level);
       if (change.elevation < level.elevation.bottom || change.elevation >= level.elevation.top) {
         const group = level.flags[`${id}`]?.group;
-        const levels = scene.levels.filter(l => (!group || group === "" || (l.flags[`${id}`]?.group === group)) && ((l.flags[`${id}`]?.inclusion || defaultLI) === "included" || (l.flags[`${id}`]?.inclusion === "default" && defaultLI === "included")));
+        const levels = scene.levels.filter(l => (((!group || group === "") && (!l.flags[`${id}`]?.group || l.flags[`${id}`]?.group === "")) || (l.flags[`${id}`]?.group === group)) && ((l.flags[`${id}`]?.inclusion || defaultLI) === "included" || (l.flags[`${id}`]?.inclusion === "default" && defaultLI === "included")));
         if (levels.length > 1) {
-          const newLevel = levels.find(l => change.elevation >= l.elevation.bottom && change.elevation < l.elevation.top);
-          if (newLevel) {
+          let newLevel = levels.find(l => change.elevation >= l.elevation.bottom && change.elevation < l.elevation.top);
+          if (!newLevel && change.elevation < level.elevation.bottom) {
+            newLevel = levels.sort((a, b) => parseFloat(a.elevation.bottom) - parseFloat(b.elevation.bottom))[0];
+          } else if (!newLevel && change.elevation > level.elevation.top) {
+            newLevel = levels.sort((a, b) => parseFloat(b.elevation.top) - parseFloat(a.elevation.top))[0];
+          }
+          if (newLevel && newLevel?.id !== level?.id) {
             const ids = canvas.tokens?.controlled?.map(c => c.document.id);
-            const delay = game.settings.get(id, 'delay') || delayDefault;
-            await new Promise(r => setTimeout(r, (delay / 8)));
+            await new Promise(r => setTimeout(r, 100));
             await token.update({ "level": newLevel.id });
             if (ids[0] === token.id) {
-              const user = game.user;
-              if (user.isGM && user.viewedLevel !== newLevel.id) {
-                await scene.view({ "level": newLevel.id });
+              if (game.settings.get(id, 'notify') || notifyDefault) {
+                const notification = game.i18n.localize("galc.notifications.change");
+                const name = newLevel.flags[`${id}`]?.navName || newLevel.name;
+                ui.notifications.info(notification.replaceAll("{token}", token.name).replaceAll("{level}", name).replaceAll("{elevation}", change.elevation).replaceAll("{units}", scene.grid.units));
               }
-              const notification = game.i18n.localize("galc.notifications.change");
-              const name = newLevel.flags[`${id}`]?.navName || newLevel.name;
-              ui.notifications.info(notification.replaceAll("{token}", token.name).replaceAll("{level}", name).replaceAll("{elevation}", change.elevation).replaceAll("{units}", scene.grid.units));
-              await new Promise(r => setTimeout(r, delay));
-              ids.forEach((i) => {
-                scene.tokens.find(t => t.id === i)?.object.control({ releaseOthers: false });
-              })
+              const observe = scene.tokens.filter(t => t.id !== token.id && t.testUserPermission(user, "OBSERVER"));
+              if (user.viewedLevel !== newLevel.id) {
+                if (user.isGM || (ids.length < 2 && observe?.length > 0 && observe.filter(t => t.level == newLevel.id)?.length < 1 && observe.filter(t => t.level == level?.id)?.length > 0)) {
+                  await scene.view({ "level": newLevel.id });
+                } else {
+                  const delay = game.settings.get(id, 'delay') || delayDefault;
+                  await new Promise(r => setTimeout(r, delay));
+                }
+                await canvas.tokens.releaseAll();
+                ids.forEach(async (i) => {
+                  await scene.tokens.find(t => t.id === i)?.object?.control({ releaseOthers: false });
+                });
+              }
             }
           }
         }
@@ -328,7 +387,7 @@ Hooks.on('renderTokenHUD', async (app, html, context) => {
         newDiv.classList.add("elevation");
         newDiv.setAttribute('data-tooltip', '');
         newDiv.setAttribute('aria-label', game.i18n.localize("galc.hud.label"));
-        newDiv.innerHTML = `<i class="caret fa-solid fa-arrows-up-to-line" inert></i><input type="text" name="absolute-elevation" value="${token.elevation || "??"}">`;
+        newDiv.innerHTML = `<i class="caret fa-solid fa-arrows-up-to-line" inert></i><input type="text" name="absolute-elevation" value="${isNaN(token.elevation) ? "??" : token.elevation}">`;
         const elem = newDiv.querySelector('input[name="absolute-elevation"]');
         elem.addEventListener('change', async (event) => {
           const value = elem?.value?.trim();
@@ -379,10 +438,10 @@ function absoluteElevation(token) {
   const flag = doc.parent.flags[`${id}`]?.abs || defaultAE;
   if (flag === "on" || (flag === "default" && defaultAE === "on")) {
     const elev = Math.round(doc.elevation);
-    if (!isNaN(elev) && elev !== 0) {
+    if (!isNaN(elev)) {
       const sign = (Math.abs(elev) === elev) ? "+" : "-";
       const unit = doc.parent.grid.units;
-      token.tooltip.text = `${sign}${elev} ${unit}`;
+      token.tooltip.text = (elev === 0) ? "" : `${sign}${elev} ${unit}`;
     }
   }
 };
@@ -437,7 +496,7 @@ Hooks.on('renderSceneNavigation', (app, html, options) => {
         const defaultLN = game.settings.get(id, 'defaultLN') || navDefault;
         const flag = l.flags[`${id}`]?.nav || defaultLN;
         const users = game.users.filter(u => u.active && u.viewedLevel === l.id && u.viewedScene === l.parent.id);
-        if (user.isGM || flag === "visible" || (flag === "default" && defaultLN === "visible") || users.map(u => u.id).includes(user.id)) {
+        if (user.isGM || flag === "visible" || (flag === "default" && defaultLN === "visible") || users.map(u => u.id).includes(user.id) || scene.tokens.filter(t => t.level === l.id && t.testUserPermission(user, "OBSERVER"))?.length > 0) {
           let players = users.length > 0 ? `<ul class="scene-players">` : ``;
           if (users.length > 0) {
             users.forEach(u => {
@@ -448,15 +507,40 @@ Hooks.on('renderSceneNavigation', (app, html, options) => {
           const view = (game.user.viewedLevel === l.id) ? `view` : ``;
           const name = l.flags[`${id}`]?.navName || l.name;
           const ariaLabel = (user.isGM && l.name !== name) ? ` data-tooltip="" aria-label="${l.name}"` : "";
+          const elevSetting = game.settings.get(id, 'navElevation') || elevationDefault;
+          const elevation = (elevSetting === "everyone" || (elevSetting === "gm" && game.user.isGM)) ? ` [${l.elevation.bottom}, ${l.elevation.top}]`.replaceAll("Infinity", "∞") : "";
           levelsStr += `<li class="level-row">
             <div class="ui-control scene scene-level ${view}" data-scene-id="${l.parent.id}" data-level-id="${l.id}" data-action="viewLevel"${ariaLabel}>
-                <span class="ellipsis">${name}</span>${players}
+                <span class="ellipsis">${name}${elevation}</span>${players}
             </div>
           </li>`;
         }
       })
       levelsNav.innerHTML = levelsStr;
       html.insertBefore(levelsNav, html.querySelector("#scene-navigation-viewed").nextSibling);
+    }
+  }
+});
+
+Hooks.on("canvasReady", () => {
+  const scene = canvas.scene;
+  if (scene?.active && (game.settings.get(id, 'preload') || preloadDefault)) {
+    const level = scene.levels.find(l => l.id === game.user.viewedLevel);
+    const group = level.flags[`${id}`]?.group;
+    const levels = scene.levels.filter(l => ((!group || group === "") && (!l.flags[`${id}`]?.group || l.flags[`${id}`]?.group === "")) || l.flags[`${id}`]?.group === group);
+    if (levels?.length > 1) {
+      const sortBottom = levels.sort((a, b) => parseFloat(a.elevation.bottom) - parseFloat(b.elevation.bottom));
+      const sortTop = levels.sort((a, b) => parseFloat(a.elevation.top) - parseFloat(b.elevation.top));
+      const indexBottom = sortBottom.findIndex(l => l.id === level.id);
+      const indexTop = sortTop.findIndex(l => l.id === level.id);
+      const lower = sortBottom[indexBottom - 1]
+      const upper = sortTop[indexTop + 1];
+      if (lower) {
+        game.scenes.preload(scene.id, { level: lower.id, broadcast: false });
+      }
+      if (upper) {
+        game.scenes.preload(scene.id, { level: upper.id, broadcast: false });
+      }
     }
   }
 });
